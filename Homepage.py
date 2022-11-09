cfd590990210101351ee163d5e7d716b0e12d695
ChiEddieCheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-09 13:59:33 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -2,8 +2,9 @@ import streamlit as st
 import time
 
 st.set_page_config(
-    page_title="Home Page",
+    page_title='Home Page',
     initial_sidebar_state='expanded'
+    layout='wide'
 )
 
 hide_menu = """
