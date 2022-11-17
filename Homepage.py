2a5e58abd64be984bfdba41d468e777ab8d36860
ChiEddieCheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-17 14:28:17 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -10,17 +10,19 @@ st.set_page_config(
 hide_menu = """
     <style>                               
         #MainMenu {visibility: hidden;}                
-        footer {visibility: visible;} 
-        footer:after{
-            content:'Updated 11/17/2022';
-            display:block;
-            position:relative;
-            
-            padding: 5px;
+       .css-qri22k.egzxvld0 {
+            visibility: hidden;
         }
+        footer:after {
+            visibility: visible;
+            content: 'Updated 11/17/2022';
+            display: block;           
+            background-color: DarkSalmon;
+            color: white;
+        }        
     </style>
 """
 st.markdown(hide_menu, unsafe_allow_html=True)
 
 st.subheader("Eddie Cheung's Python Learning Project")
-st.info('##### \N{clipboard} On the left sidebar contains a list of apps written in Streamlit Python')
+st.info('##### \N{clipboard} Sidebar menu contains a list of apps written in Streamlit Python')
