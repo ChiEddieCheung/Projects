47711c2f15d45115cfb7035873c45493e7c02058
ChiEddieCheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-18 01:54:27 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -15,7 +15,7 @@ hide_menu = """
         }
         footer:after {
             visibility: visible;
-            content: "url(https://my.indeed.com/p/chichiueddiec-5mgjx37)"
+            content: "My bio url(https://my.indeed.com/p/chichiueddiec-5mgjx37)"
             font-style: italic;
             display: block;    
             text-align: center;
