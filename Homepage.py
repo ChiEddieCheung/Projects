9ffb74d96782290c4929e29d21471bcd2a06af0e
ChiEddieCheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-18 01:56:12 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -15,7 +15,7 @@ hide_menu = """
         }
         footer:after {
             visibility: visible;
-            content: "My bio"
+            content: 'My bio'
             font-style: italic;
             display: block;    
             text-align: center;
