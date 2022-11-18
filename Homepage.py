87f06814241ab621e19672a4b75a725d670ab76b
ChiEddieCheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-18 03:17:57 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -32,5 +32,5 @@ st.markdown(hide_menu, unsafe_allow_html=True)
 st.subheader("Eddie Cheung's Python Learning Project")
 st.info('##### \N{clipboard} Sidebar menu contains a list of apps written in Streamlit Python')
 
-st.success("#### [Feel free to check out my bio](https://my.indeed.com/p/chichiueddiec-5mgjx37)")
+st.success("##### \N{clipboard} [Feel free to check out my bio](https://my.indeed.com/p/chichiueddiec-5mgjx37)")
     #components.iframe("https://docs.streamlit.io/en/latest", scrolling=True)
