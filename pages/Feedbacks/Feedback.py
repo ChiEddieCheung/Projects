7c41f80ca6e4b5424749bf758b89f964adf6cb2f
ChiEddieCheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-21 23:04:19 -0600

    Update Feedback.py

--- pages/Feedbacks/Feedback.py
+++ pages/Feedbacks/Feedback.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import sqlite3
+#import sqlite3
 
 hide_menu = """
     <style>
@@ -10,15 +10,15 @@ hide_menu = """
 """
 st.markdown(hide_menu, unsafe_allow_html=True)
 
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
+#conn = sqlite3.connect('data.db')
+#c = conn.cursor()
 
-def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS blogtable(author TEXT,title TEXT,article TEXT,postdate,DATE)')
+#def create_table():
+#    c.execute('CREATE TABLE IF NOT EXISTS blogtable(author TEXT,title TEXT,article TEXT,postdate,DATE)')
 
-def add_data(author,title,article,postdate):
-    c.execute('INSERT INTO blogtable(author,title,article,postdate) VALUES (?,?,?,?)',(author,title,article,postdate))
-    conn.commit()
+#def add_data(author,title,article,postdate):
+#    c.execute('INSERT INTO blogtable(author,title,article,postdate) VALUES (?,?,?,?)',(author,title,article,postdate))
+#    conn.commit()
 
 html_temp = """
     <div style="background-color:{};padding:5px;border-radius:8px">
@@ -29,7 +29,7 @@ st.markdown(html_temp.format('lightblue','black'),unsafe_allow_html=True)
 
 st.write("###### *Please leave any comment or idea on our feedback form.*")
 st.write('###### *Your feedback is very important to me!*')
-create_table()
+#create_table()
 
 with st.form('myForm', clear_on_submit=True):
     blog_title = st.text_input('Enter Your Topic:')
@@ -38,5 +38,5 @@ with st.form('myForm', clear_on_submit=True):
     blog_post_date = st.date_input("Post Date")
     submit_button = st.form_submit_button('Add')    
     if submit_button:
-        add_data(blog_author,blog_title,blog_article,blog_post_date)
+#        add_data(blog_author,blog_title,blog_article,blog_post_date)
         st.success("Post: '{}' is saved".format(blog_title))
