dad13eeada8f13fdd584ff1b3cfcbfb843dd69e4
ChiEddieCheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-23 15:11:32 -0600

    Update Feedback Form.py

--- pages/Feedback Form.py
+++ pages/Feedback Form.py
@@ -25,7 +25,7 @@ def add_data(author,title,article,postdate):
 
 html_temp = """
     <div style="background-color:{};padding:5px;border-radius:8px">
-    <h1 style="color:{};text-align: center;font-size: 28px">Add Feedback</h1>
+    <h1 style="color:{};text-align: center;font-size: 28px">Feedback Form</h1>
     </div><br>
 """
 st.markdown(html_temp.format('lightblue','black'),unsafe_allow_html=True)
