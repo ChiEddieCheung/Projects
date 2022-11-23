19bca6f94d53b0f445f788ff737fd251589cb3bd
ChiEddieCheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-22 21:24:52 -0600

    Update Test.py

--- pages/Test.py
+++ pages/Test.py
@@ -1,20 +1,21 @@
 import streamlit as st
 import mysql.connector as mqc
 
-@st.experimental_singleton
-def init_connection():
-    return mqc.connect(**st.secrets["mysql"])
+#@st.experimental_singleton
+#def init_connection():
+#    return mqc.connect(**st.secrets["mysql"])
 
-conn = init_connection()
+#conn = init_connection()
 
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+#@st.experimental_memo(ttl=600)
+#def run_query(query):
+#    with conn.cursor() as cur:
+#        cur.execute(query)
+#        return cur.fetchall()
 
-rows = run_query("SELECT * FROM mytable;")
+#rows = run_query("SELECT * FROM mytable;")
 
-for row in rows:
-    # Emoji name syntax :{emoji name}:
-    st.write(f"{row[0]} has a :{row[1]}:")
+#for row in rows:
+#    # Emoji name syntax :{emoji name}:
+#    st.write(f"{row[0]} has a :{row[1]}:")
+st.write('Test')
