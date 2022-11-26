c57d9e89fb90a2d91a1561afa201fed8f0657687
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-26 00:48:09 -0600

    Update 5_Stock Sentiment.py

--- pages/5_Stock Sentiment.py
+++ pages/5_Stock Sentiment.py
@@ -54,7 +54,7 @@ if search == True or symbol != '':
                 df = df.append(pd.DataFrame(row).T)
             
         df = df.rename(columns = {0:'Date Created', 1:'Twitter Name', 2:'Followers', 3:'Sentiment'})
-        df.style.applymap(HiLightCells, subset=['Followes'])
+        df.style.applymap(HiLightCells, subset=['Followers'])
         df['Date Created'] = pd.to_datetime(df['Date Created']).dt.date
         df = df.reset_index().drop(['index'], axis=1)
         st.dataframe(df, use_container_width=True)
