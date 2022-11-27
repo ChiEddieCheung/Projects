6bf745e17287153fb0a531ea28966aa0e1d272be
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-27 01:24:01 -0600

    Update 6_Stock Financial Dashboard.py

--- pages/6_Stock Financial Dashboard.py
+++ pages/6_Stock Financial Dashboard.py
@@ -31,7 +31,7 @@ class Company:
        
         self.year_end = overview_df.loc['Fiscal Year Ends'][0]        
         self.market_cap = overview_dict['Market Cap']
-        temp = str(self.market_cap)
+        temp = self.market_cap
         st.write(temp)
         self.market_cap = '${}'.format(self.market_cap)
         
