5cdbf483d1be77394ba6ad58741ff992b544c76f
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-27 02:15:17 -0600

    Update 6_Stock Financial Dashboard.py

--- pages/6_Stock Financial Dashboard.py
+++ pages/6_Stock Financial Dashboard.py
@@ -44,6 +44,7 @@ class Company:
         st.write(val1)
         st.write(val2)
         st.write(val1 + val2)
+        self.market_cap = val1 + val2
         #self.market_cap = '${}'.format(self.market_cap)
         
         self.prices = price_df['adjclose']
