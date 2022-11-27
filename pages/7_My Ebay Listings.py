b9cfaa6ae9e0bd3657b15c1d427124a8c778c8ba
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-27 10:55:34 -0600

    Update 7_My Ebay Listings.py

--- pages/7_My Ebay Listings.py
+++ pages/7_My Ebay Listings.py
@@ -19,10 +19,6 @@ html_temp = """
     <h1 style="color:{};text-align: center;font-size: 28px">My EBay Listings</h1>
     </div><br>
 """
-st.markdown(html_temp.format('lightblue','black'),unsafe_allow_html=True)
-
-st.write('LEGO Speed Champions: Ferrari F40 Competizione (75890))'
-
-#st.markdown('<a href="https://www.ebay.com/itm/403908078579?mkcid=16&mkevt=1&mkrid=711-127632-2357-0&ssspo=hd1-ldmkqjm&sssrc=2051273&ssuid=hd1-ldmkqjm&var=&widget_ver=artemis&media=MORE">LEGO Speed Champion</a>', unsafe_allow_html=True)
+st.markdown(html_temp.format('lightblue','black'), unsafe_allow_html=True)
 
 components.iframe('https://www.google.com")
