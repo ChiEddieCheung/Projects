5a2a0c55120b0b5fef83f29369e8cd1d8e9546eb
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-30 00:17:20 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -19,7 +19,7 @@ hide_menu = """
             font-style: italic;
             display: block;    
             text-align: center;
-            background: #ffdd00;
+            background: #ffee00;
             color: black;
             padding: 5px;
             height: 35px;
