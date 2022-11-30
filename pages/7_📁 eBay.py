61489bbe5bf753b25a9467e3d5be540c9fe832db
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-29 20:07:07 -0600

    Update 7_📁 eBay.py

--- pages/7_📁 eBay.py
+++ pages/7_📁 eBay.py
@@ -5,17 +5,18 @@ from PIL import Image
 st.caption('*Page under construction*')
 st.title('My eBay Listings')
 
+col1, col2 = st.columns(2)
 
 sImg = "https://i.ebayimg.com/images/g/m-EAAOSwl1diWZJN/s-l500.jpg"
 sImg2 = "https://i.ebayimg.com/images/g/Ha0AAOSwg8FiWZJV/s-l500.jpg"
 
-showImg = f"""
-    <img src={sImg} 
-     width="54" height="80" alt="LEGO Harry Potter">
-"""
-st.markdown(showImg, unsafe_allow_html=True)
+with col1:
+    showImg = f"""
+        <img src={sImg} 
+        width="54" height="80" alt="LEGO Harry Potter">
+    """
+    st.markdown(showImg, unsafe_allow_html=True)
 
-st.write('---')
-
-st.image(sImg2, width=54)
+with col2:
+    st.image(sImg2, width=80)
 
