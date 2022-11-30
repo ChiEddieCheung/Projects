477e0cffe3eca5c1ae6f33aae5efc70c7571b749
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-29 19:52:23 -0600

    Update 7_📁 eBay.py

--- pages/7_📁 eBay.py
+++ pages/7_📁 eBay.py
@@ -8,7 +8,7 @@ st.title('My eBay Listings')
 
 sImg = "https://i.ebayimg.com/images/g/m-EAAOSwl1diWZJN/s-l500.jpg"
 showImg = """
-    <img src="{sImg}" 
+    <img src={sImg} 
      width="40" height="60" alt="LEGO Harry Potter">
 """
 st.markdown(showImg, unsafe_allow_html=True)
