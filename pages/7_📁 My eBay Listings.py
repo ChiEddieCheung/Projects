c766510f6a96800c0a92e6288f89a14da9c7d309
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-29 20:55:04 -0600

    Update 7_📁 My eBay Listings.py

--- pages/7_📁 My eBay Listings.py
+++ pages/7_📁 My eBay Listings.py
@@ -8,13 +8,15 @@ st.title('My eBay Listings')
 sImg = "https://i.ebayimg.com/images/g/m-EAAOSwl1diWZJN/s-l500.jpg"
 sImg2 = "https://i.ebayimg.com/images/g/Ha0AAOSwg8FiWZJV/s-l500.jpg"
 
-val = st.slider('Select a picture to view:', 1, 2, 1, format=f'Pic {val}')
+val = st.slider('Slide to select a picture to view:', 1, 2, 1)
 if val == 1:
+    st.caption('Picture 1 of 2')
     showImg = f"""
         <img src={sImg} 
         width="54" height="80" alt="LEGO Harry Potter">
     """
     st.markdown(showImg, unsafe_allow_html=True)
 else:
+    st.caption('Picture 2 of 2')
     st.image(sImg2, width=80)
 st.caption('LEGO Harry Potter Hogwarts Moment')
