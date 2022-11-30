f305c9ca1a5287187803e2614fa95d97cd4b79c8
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-29 20:45:21 -0600

    Update 7_📁 My eBay Listings.py

--- pages/7_📁 My eBay Listings.py
+++ pages/7_📁 My eBay Listings.py
@@ -1,21 +1,14 @@
 import streamlit as st
 from bs4 import BeautifulSoup
 from PIL import Image
-from datetime import datetime
 
 st.caption('*Page under construction*')
 st.title('My eBay Listings')
 
-start_time = st.slider(
-    "When do you start?",
-    value=datetime(2020, 1, 1, 9, 30),
-    format="MM/DD/YY - hh:mm")
-st.write("Start time:", start_time)
-
 sImg = "https://i.ebayimg.com/images/g/m-EAAOSwl1diWZJN/s-l500.jpg"
 sImg2 = "https://i.ebayimg.com/images/g/Ha0AAOSwg8FiWZJV/s-l500.jpg"
 
-val = st.slider('Select a picture to view:', 1, 2, 1, format='Pic ' + val)
+val = st.slider('Select a picture to view:', 1, 2, 1, format='Pic {}')
 if val == 1:
     showImg = f"""
         <img src={sImg} 
