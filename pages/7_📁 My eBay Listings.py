0300ed2c20bfa87a9f24569974d69d1ecc34ef35
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-29 21:03:27 -0600

    Update 7_📁 My eBay Listings.py

--- pages/7_📁 My eBay Listings.py
+++ pages/7_📁 My eBay Listings.py
@@ -8,18 +8,22 @@ st.title('My eBay Listings')
 sImg = "https://i.ebayimg.com/images/g/m-EAAOSwl1diWZJN/s-l500.jpg"
 sImg2 = "https://i.ebayimg.com/images/g/Ha0AAOSwg8FiWZJV/s-l500.jpg"
 
-with st.container():
-    st.subheader('Slide to select a picture to view:')
+col1, col2 = st.columns(2, 5)
+
+with col1:
+    st.write('Slide to select a picture to view:')
     val = st.slider('Slide to select a picture to view:', 1, 2, 1, label_visibility='hidden')
     
-if val == 1:
-    st.caption('Picture 1 of 2')
-    showImg = f"""
-        <img src={sImg} 
-        width="54" height="80" alt="LEGO Harry Potter">
-    """
-    st.markdown(showImg, unsafe_allow_html=True)
-else:
-    st.caption('Picture 2 of 2')
-    st.image(sImg2, width=80)
-st.caption('LEGO Harry Potter Hogwarts Moment')
+    if val == 1:
+        st.caption('Picture 1 of 2')
+        showImg = f"""
+            <img src={sImg} 
+            width="54" height="80" alt="LEGO Harry Potter">
+        """
+        st.markdown(showImg, unsafe_allow_html=True)
+    else:
+        st.caption('Picture 2 of 2')
+        st.image(sImg2, width=80)
+
+with col2:
+    st.caption('LEGO Harry Potter Hogwarts Moment')
