414ee5e4158b8791c4a15caf7f564c88955709d8
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-30 11:47:12 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -28,14 +28,13 @@ hide_menu = """
 """
 st.markdown(hide_menu, unsafe_allow_html=True)
 
-urlImg = "https://github.com/ChiEddieCheung/Projects/blob/main/abstract.jpg?raw=true"
-
+ImgUrl = "https://github.com/ChiEddieCheung/Projects/blob/main/abstract.jpg?raw=true"
 def add_bg_from_url():
     st.markdown(
          f"""
          <style>
          .stApp {{
-             background-image: url("{urlImg}");
+             background-image: url("{ImgUrl}");
              background-attachment: fixed;
              background-size: cover;
          }}
@@ -43,17 +42,13 @@ def add_bg_from_url():
          """,
          unsafe_allow_html=True
      )
-
 add_bg_from_url() 
 
 with st.form('myform'):
     st.header("Eddie Cheung's Python Learning Project")
 
     st.write('')
-    st.write('###### \N{clipboard} Sidebar menu contains a list of apps written in Streamlit Python')
-    
-    st.text('This is text.')
-    st.caption('This is caption.')
+    st.write('###### \N{clipboard} Sidebar menu contains a list of apps written in Streamlit Python')       
 
     for i in range(5):
         st.write('')
@@ -66,6 +61,6 @@ with st.form('myform'):
     if send:
         st.markdown("""
             <head>
-              <meta http-equiv="refresh" content="0; URL='https://my.indeed.com/p/chichiueddiec-5mgjx37'"/>
+              <meta http-equiv="refresh" content="5; URL='https://my.indeed.com/p/chichiueddiec-5mgjx37'"/>
             </head>
           """, unsafe_allow_html=True)
