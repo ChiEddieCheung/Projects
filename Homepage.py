6765e0f2281902720d656a026b25cf7efa31fdad
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-30 13:42:01 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -35,7 +35,7 @@ def add_bg_from_url():
          <style>
          .stApp {{
              background-image: url("{ImgUrl}");     
-             background-size: cover;
+             background-size: contain;
              background-attachment: fixed;
          }}
          </style>
