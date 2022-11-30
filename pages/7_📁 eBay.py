c11220fdd34bec840549f1f16b7cd72e77df8f9a
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-29 19:41:06 -0600

    Update 7_📁 eBay.py

--- pages/7_📁 eBay.py
+++ pages/7_📁 eBay.py
@@ -8,6 +8,7 @@ st.title('My eBay Listings')
 showImg = """
     <img src="https://i.ebayimg.com/images/g/m-EAAOSwl1diWZJN/s-l500.jpg" 
      width="60" height="40">
+"""
 st.markdown(showImg, unsafe_use_html=True)
 st.write('---')
 st.image('https://i.ebayimg.com/images/g/m-EAAOSwl1diWZJN/s-l500.jpg')
