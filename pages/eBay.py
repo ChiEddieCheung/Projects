6416087d1704478262eff04f8003e7f3a8367d48
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-11-29 19:09:35 -0600

    Update eBay.py

--- pages/eBay.py
+++ pages/eBay.py
@@ -2,5 +2,5 @@ import streamlit as st
 from bs4 import BeautifulSoup
 from PIL import Image
 
-st.caption(*Page under construction*)
+st.caption('*Page under construction*')
 st.title('My eBay Listings')
