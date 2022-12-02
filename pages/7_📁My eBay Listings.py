5022d0e83654f916e9d62f123bc0188bdedb3bcf
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-12-02 17:02:34 -0600

    Update 7_📁My eBay Listings.py

--- pages/7_📁My eBay Listings.py
+++ pages/7_📁My eBay Listings.py
@@ -2,7 +2,7 @@ import streamlit as st
 from bs4 import BeautifulSoup
 #from PIL import Image
 import pyqrcode
-#import cv2
+import png
 
 hide_menu = """
     <style>
@@ -47,8 +47,8 @@ with col2:
         </a>
     """, unsafe_allow_html=True)
     
-    #st.write('')
-    #url = pyqrcode.create(sSite)        
-    #file_name = '403826614108'               
-    #url.png(f'ebay{file_name}.png', scale = 6)
+    st.write('')
+    url = pyqrcode.create(sSite)        
+    file_name = '403826614108             
+    url.png(f'ebay{file_name}.png', scale = 6)
     #st.image(f'ebay{file_name}', width=200)
