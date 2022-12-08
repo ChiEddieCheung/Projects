fcfd37a4adc3343b3b542ddb2cc6937f1666e06f
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-12-08 10:52:38 -0600

    Update 6_📁 Stock Financial Dashboard.py

--- pages/6_📁 Stock Financial Dashboard.py
+++ pages/6_📁 Stock Financial Dashboard.py
@@ -73,7 +73,7 @@ if search or ticker:
     if 'Price' in st.session_state:
         Stock_Price = '$' + st.session_state['Price']
     else:
-        Stock_Price = '$' + si.get_quote_data(ticker)['regularMarketPrice']
+        Stock_Price = si.get_quote_data(ticker)['regularMarketPrice']
     st.success(f"##### {si.get_quote_data(ticker)['shortName']} \n {Stock_Price}")
         
     st.write('##### Company Overview')
