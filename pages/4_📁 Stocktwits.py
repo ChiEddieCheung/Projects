232c257b5c7b05f3b58fea1bafe3262ee4749619
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-12-07 21:58:19 -0600

    Update 4_📁 Stocktwits.py

--- pages/4_📁 Stocktwits.py
+++ pages/4_📁 Stocktwits.py
@@ -25,7 +25,8 @@ search = st.button('Search', key={symbol})
 if symbol or search:
     try:
         r = requests.get(f"https://api.stocktwits.com/api/2/streams/symbol/{symbol}.json")
-        data = r.json()    
+        data = r.json() 
+        st.write(data)
         
         st.success(f"###### {data['symbol']['title']}")        
 
