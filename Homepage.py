37706bf51b8623fbd2c2a40e6736bbaa71916016
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2022-12-25 20:37:34 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -39,7 +39,7 @@ def add_background():
              background-attachment: fixed;
              background-size: cover; 
              background-repeat: repeat;
-             background-color: #ffffdd;
+             background-color: #ffff0d;
          }}
          </style>
          """,
