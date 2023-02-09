92fd61e836b42ac69cf8b680b759703fad409478
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2023-02-09 09:30:40 -0600

    Update 6_📁 Stocktwits.py

--- pages/6_📁 Stocktwits.py
+++ pages/6_📁 Stocktwits.py
@@ -30,35 +30,34 @@ with colTicker:
     
 search = st.button('Search', key={symbol})
 if symbol:
-    try:
-        r = requests.get(f"https://api.stocktwits.com/api/2/streams/symbol/{symbol}.json")
-        data = r.json() 
+    r = requests.get(f"https://api.stocktwits.com/api/2/streams/symbol/{symbol}.json")
+    data = r.json() 
                
-        stock = yf.Ticker(symbol)          
-        info = stock.info
-        st.write(info)
-                                
-        imgUrl = info['logo_url']
-        last_price = round(stock.fast_info['lastPrice'], 2)
-        prev_close = stock.fast_info['previousClose']
-          
-        st.success(info['shortName'])
-        
-        col1, col2 = st.columns([1,4])
-        with col1:
-            st.image(imgUrl)
-        with col2:
-            st.metric(label='', value=f'${last_price}', 
-            delta=round(last_price - prev_close, 2))
+    stock = yf.Ticker(symbol)          
+    info = stock.info
+    st.write(info)
 
-        st.write('___')
-        for message in data['messages']:                  
-            st.image(message['user']['avatar_url'])
-            st.write(message['user']['username'])        
-            st.write(message['created_at'])
-            st.write(message['body'])        
-            st.write('___')                
-    except:
-        st.write('\N{cross mark} Stock ticker not found!')
+    imgUrl = info['logo_url']
+    last_price = round(stock.fast_info['lastPrice'], 2)
+    prev_close = stock.fast_info['previousClose']
+          
+    st.success(info['shortName'])
+
+    col1, col2 = st.columns([1,4])
+    with col1:
+        st.image(imgUrl)
+    with col2:
+        st.metric(label='', value=f'${last_price}', 
+        delta=round(last_price - prev_close, 2))
+
+    st.write('___')
+    for message in data['messages']:                  
+        st.image(message['user']['avatar_url'])
+        st.write(message['user']['username'])        
+        st.write(message['created_at'])
+        st.write(message['body'])        
+        st.write('___')                    
+    
+    #st.write('\N{cross mark} Stock ticker not found!')
 
     del st.session_state['symbol']
