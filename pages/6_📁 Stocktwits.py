c109e7078d50b7e33b9b30cded3ff9e5e0ac3e4f
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2023-02-09 09:26:22 -0600

    Update 6_📁 Stocktwits.py

--- pages/6_📁 Stocktwits.py
+++ pages/6_📁 Stocktwits.py
@@ -35,8 +35,8 @@ if symbol:
         data = r.json() 
                
         stock = yf.Ticker(symbol)          
-        #info = stock.basic_info   
-        st.write(stock)
+        info = stock.info()
+        st.write(info)
                                 
         imgUrl = info['logo_url']
         last_price = round(stock.fast_info['lastPrice'], 2)
