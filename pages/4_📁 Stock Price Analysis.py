6b8db3b52d012a5922deb1a62268026de8f2f24b
Eddie Cheung <117769049+ChiEddieCheung@users.noreply.github.com>
2023-02-09 21:37:32 -0600

    Update 4_📁 Stock Price Analysis.py

--- pages/4_📁 Stock Price Analysis.py
+++ pages/4_📁 Stock Price Analysis.py
@@ -42,14 +42,14 @@ search = st.button('Search', key={ticker})
 
 if ticker:    
     stock = yf.Ticker(ticker)          
-    info = stock.get_info()        
+    #info = stock.get_info()        
     #imgUrl = info['logo_url']
     
     #price = info['regularMarketPrice']
     price = round(stock.fast_info['last_price'], 2)
     prev_close = stock.fast_info['previous_close']
     
-    st.success(info['shortName'])
+    st.success(stock.fast_info['shortName'])
     
     col1, col2 = st.columns([1,4])
     #with col1:
